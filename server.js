8dc5016c13e44862d92d9834fb6df9ebcceb99f6
ilyesbenbrahim <benbrahim1.ilyes@gmail.com>
2024-04-25 14:16:25 -0400

    connected with database

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 // console.log("hi");
 const express = require("express");
-const app = express();
+const {MongoClient} = require("mongodb");
 
+const url = "mongodb+srv://benbrahimahcene:p7MkOfNzM5V9g7rt@cluster0.q5rzqzs.mongodb.net/initial";
+
+
+const client = new MongoClient(url)
+
+client.connect()
+.then(async()=>{
+  console.log("ok")
+  const databse = client.db('initial')
+  const collection = databse.collection('user')
+  const user = await collection.find({}).toArray()
+  console.log(user)
+})
+
+const app = express();
 app.get("/", (req, res) => {
   res.send("hello node api ");
 });
@@ -13,3 +28,7 @@ app.get("/blog", (req, res) => {
 app.listen(3000, () => {
   console.log("node api app is running on port 3000");
 });
+
+
+
+
